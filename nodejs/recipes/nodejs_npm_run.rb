2675561540fc6457f5d2f92aa9889a25dae57c4c
brian@Z97X <brian@jongensvantechniek.nl>
2016-05-24 16:08:11 +0200

    Refactor file name to reflect future feature to inject json to customize tasks that need to be run

--- nodejs/recipes/nodejs_npm_run.rb
+++ nodejs/recipes/nodejs_npm_run.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
-    if node[:custom_env][application.to_s]['npm_task']
-        execute 'nodejs_npm_install' do
+    if node[:custom_env][application.to_s]['npm_run']
+        execute 'nodejs_npm_run' do
             user    'root'
             cwd     "#{deploy[:deploy_to]}/current"
             command 'npm run build:prod'
